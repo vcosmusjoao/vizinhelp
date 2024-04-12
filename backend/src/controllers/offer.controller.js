55b7a8032e5f8061879b8c06a93dd9322fe912c7
joao <joaodevcosta@gmail.com>
2024-04-12 18:35:51 -0300

    Criando endpoint de todas as ofertas, oferta por id e oferta do usuário

--- backend/src/controllers/offer.controller.js
+++ backend/src/controllers/offer.controller.js
@@ -1,6 +1,5 @@
 const db = require("../config/database");
 
-// Método responsável por criar uma nova oferta
 exports.createOffer = async (req, res) => {
   try {
     // Extrair os dados da requisição
@@ -36,5 +35,54 @@ exports.createOffer = async (req, res) => {
     // Enviar uma resposta de erro em caso de falha
     console.error("Error creating offer:", error);
     res.status(500).json({ error: "Failed to create offer" });
+  }
+};
+
+exports.getAllOffers = async (req, res) => {
+  try {
+      const { rows } = await db.query("SELECT * FROM offers");
+      res.status(200).json(rows);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getOfferById = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const { rows } = await db.query("SELECT * FROM offers WHERE offer_id = $1", [id]);
+      if (rows.length === 0) {
+          return res.status(404).json({ message: "Oferta não encontrada" });
+      }
+      res.status(200).json(rows[0]);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getUserOffers = async (req, res) => {
+  try {
+      // Obter o ID do usuário atualmente autenticado
+      const userkeycloak = req.tokenData.sub;
+
+      const { rows: userRows } = await db.query(
+        "SELECT user_id FROM users WHERE keycloak_user_id = $1",
+        [userkeycloak]
+      );
+      
+      if (userRows.length === 0) {
+        // Se o usuário não for encontrado, você pode tratar o erro aqui
+        return res.status(404).send({ message: "User not found" });
+      }
+
+      const useridEntity = userRows[0].user_id;
+
+
+      // Consulta para buscar as ofertas do usuário pelo ID
+      const { rows } = await db.query("SELECT * FROM offers WHERE user_id = $1", [useridEntity]);
+
+      res.status(200).json(rows);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
   }
 };
