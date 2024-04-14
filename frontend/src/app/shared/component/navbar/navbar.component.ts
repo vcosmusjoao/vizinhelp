0a9f1078a2e80901d13d9b537ccca62f5b078974
vcosmusjoao <joaodevcosta@gmail.com>
2024-04-14 13:09:33 -0300

    Criação dos primeiros componentes e rotas

---
+++ frontend/src/app/shared/component/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService) { }
+  
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isLoggedIn();
+  }
+
+  onLogin(): void {
+    this.authService.login();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+
+}
